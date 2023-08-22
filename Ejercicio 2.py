dc52ca7f2528b3e5edd61f7db7a6ef259eec28ab
JuanEstebanAG <estebanart11@gmail.com>
2023-08-22 17:23:29 -0500

    version 1

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,39 +1,4 @@
-#Clase para almacenar
-class Sistema:
-    #Database donde voy a almacenar los datos
-    def __init__(self):
-        self.database = []
-        #Para que no se modifique y dependa solo del tamaño de la lista
-        self.numberPatient = len(self.database)
-    
-    def insertPatient(self):
-        name = input('Ingrese el nombre: ')   
-        cc = int(input('Digite la cédula: '))
-        gender = input('Ingrese el género: ')
-        service = input('Ingrese el servicio: ')
-        p = Paciente()
-        p.assignName(name)
-        p.assignCc(cc)
-        p.assignGender(gender)
-        p.assignService(service)
-        
-        self.database.append(p)
-        self.numberPatient = len(self.database)
-    
-    def showNumberPatient(self):
-        return self.numberPatient
-    
-    def showDataPatient(self):
-        cc = int(input('Digite la cédula que está buscando: '))
-        for patient in self.database:
-            if cc == Paciente.showCc(self):
-                print(f'''
-                    Nombre:  {patient.showName()}
-                    Cédula: {str(patient.showCc())}
-                    Género: {patient.showGender()}
-                    Servicio: {patient.showService()}''')
-
-#Clase que almacenaré
+# Clase que almacenaré
 class Paciente:
     def __init__(self):
         self.name = ""
@@ -41,8 +6,7 @@ class Paciente:
         self.gender = ""
         self.service = ""
     
-    
-    #MÉTODO VER
+    # MÉTODO VER
     def showName(self):
         return self.name
     def showService(self):
@@ -52,34 +16,78 @@ class Paciente:
     def showCc(self):
         return self.cc
     
-    #MÉTODO ASIGNAR
+    # MÉTODO ASIGNAR
     def assignName(self, n):
         self.name = n
     def assignService(self, s):
-        self.name = s
+        self.service = s
     def assignGender(self, g):
-        self.name = g
+        self.gender = g
     def assignCc(self, c):
-        self.name = c
-            
+        self.cc = c
+
+# Clase para almacenar
+class Sistema:
+    # Database donde voy a almacenar los datos
+    def __init__(self):
+        self.database = []
     
+    def insertPatient(self, pat):
+        self.database.append(pat)
+        return True  #Retorna True si el paciente se insertó correctamente
+        
+    def showNumberPatient(self):
+        print(f'En la base de datos hay: {str(len(self.database))} pacientes.')
+    
+    def showDataPatient(self, c):
+        for p in self.database:
+            if c == p.showCc():
+                return p
 
-system = Sistema()
+def main():
+    sis = Sistema()
+    while True:
+        menu = input('''Elija la opción que desea realizar:
+                     1. Ingresar nuevo paciente.
+                     2. Ver datos del paciente. 
+                     3. Ver número de pacientes.
+                     4. Salir.
+                     -> ''')
+        if menu == '1':
+            name = input('Nombre: ')
+            cc = int(input('Cédula: '))
+            gender = input('Género: ')
+            service = input('Servicio: ')
+            pat = Paciente()
+            pat.assignCc(cc)
+            pat.assignGender(gender)
+            pat.assignName(name)
+            pat.assignService(service)
+            success = sis.insertPatient(pat)
+            if success:
+                print('\nPaciente insertado correctamente.')
+            else:
+                print('\nError al insertar el paciente.')
 
-while True:
-    menu =  input('''\nSeleccione la opción que desea realizar:
-                1. Ingresar nuevo paciente.
-                2. Ver número de pacientes.
-                3. Ver datos del paciente.
-                4. Salir.
-                -> ''')
-    if menu == '1':
-        system.insertPatient()
-    elif menu == '2':
-        print(f'\nActualmente existen: {system.showNumberPatient()} pacientes.')
-    elif menu == '3':
-        system.showDataPatient()
-    elif menu == '4':
-        break
-    else:
-        print('\nSeleccione una opción válida.')
+        elif menu == '2':
+            c = int(input('\nDigite la cédula que está buscando: '))
+            p = sis.showDataPatient(c)
+            print(f'''
+                Datos del paciente:
+                Nombre: {p.showName()}
+                Cédula: {str(p.showCc())}
+                Género: {p.showGender()}
+                Servicio: {p.showService()}''')
+        
+        elif menu == '3':
+            sis.showNumberPatient()
+        
+        elif menu == '4':
+            print('Hasta la próxima.') #Decoración jajaja
+            break
+        
+        else:
+            print('S\neleccione una opción válida.')
+
+if __name__ == "__main__":
+    main()
